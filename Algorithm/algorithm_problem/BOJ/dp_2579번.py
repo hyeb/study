ef33158f55bca70fd123fe6a79a64e210accbbde
hyeb <hyebb05512@gmail.com>
2022-08-03 16:29:38 +0900

    second commit

--- Algorithm/algorithm_problem/BOJ/dp_2579번.py
+++ Algorithm/algorithm_problem/BOJ/dp_2579번.py
@@ -9,10 +9,19 @@ N = int(input())
 stairs = [int(input()) for _ in range(N)]
 
 dp = [0] * N
-dp[0] = stairs[0]
-dp[1] = max(stairs[1], stairs[1]+stairs[0])
-dp[2] = max(stairs[0]+stairs[2],stairs[1]+stairs[2])
 
-for i in range(3, N):
-    dp[i] = max(stairs[i]+stairs[i-1]+dp[i-3], stairs[i]+dp[i-2])
-print(max(dp))
+if N == 1:
+    print(stairs[0])
+elif N == 2:
+    print(stairs[0] + stairs[1])
+else:
+    dp[0] = stairs[0]
+    dp[1] = stairs[1]+stairs[0]
+    dp[2] = max(stairs[0]+stairs[2],stairs[1]+stairs[2])
+
+    for i in range(3, N):
+        dp[i] = max(stairs[i]+stairs[i-1]+dp[i-3], stairs[i]+dp[i-2])
+    print(dp[N-1])
+
+## +) N이 1,2 일 케이스를 따로 고려해주지않아서 여러번 틀림..
+## +) 마지막에 max(dp)가 아니라 마지막 계단 즉, max(dp[N-1])로 고려해줘야 한다는 부분에서도..
