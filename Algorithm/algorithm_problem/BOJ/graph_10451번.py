123963bbbcd8bdd58086a30d094787f57ab665bc
hyeb <hyebb05512@gmail.com>
2022-08-11 00:03:20 +0900

    second commit

--- Algorithm/algorithm_problem/BOJ/graph_10451번.py
+++ Algorithm/algorithm_problem/BOJ/graph_10451번.py
@@ -1,30 +1,32 @@
 # 백준 10451번 순열 사이클
 # https://www.acmicpc.net/problem/10451
 
-def cycle_check(visited, num, start, count):
-    visited[num] = True
-    count += 1
-    # 이미 방문한 상태고 처음 시작 숫자와 같다면 사이클 완성이므로 반환
-    if visited[num-1] == True and visited[num] == start:
-        return count
+import sys
+sys.setrecursionlimit(10**7)
 
-    if False not in visited: # 모두 한번씩 방문해봤다면 빠져나오기
-        return
-    
-    cycle_check(visited, num) # 아니라면 계속 체크하기
+def cycle_check(num):
+    visited[num] = True
+    next = array[num]
+    if visited[next] == False:
+        cycle_check(next)
 
 T = int(input())
 
 for _ in range(T):
     N = int(input())
-    array = list(map(int, input().split()))
+    array = [0] + list(map(int, input().split()))
     res = 0
-    visited = [False] * (len(array)+1)
-    visited[0] = True
-    for i in range(len(array)):
-        count = 0
-        start = i+1
-        visited[i+1] = True
-        res += cycle_check(visited, array[i], start, count)
+    visited = [False] * (N+1)
+
+    for i in range(1, N+1):
+        if visited[i] == False:
+            cycle_check(i) 
+            res += 1 # 빠져나오면 그게 1 사이클이기 때문에 +1
 
     print(res)
+
+
+# N이 주어졌는데 왜 len으로 하고있었지 ㅋㅋㅋ
+# 초반의 기본 아이디어는 잘 떠올렸지만 중간에 생각이 조금 꼬인듯하다
+
+## 런타임에러가 났다.. 제한리밋 풀어줘야함
