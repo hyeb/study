2270d4bb9df5ea2ce0073232200122208cd869ff
hyeb <hyebb05512@gmail.com>
2022-08-16 21:22:53 +0900

    third commit

--- Algorithm/algorithm_problem/LeetCode/N399.py
+++ Algorithm/algorithm_problem/LeetCode/N399.py
@@ -4,36 +4,42 @@
 from collections import defaultdict, deque
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
         dic = defaultdict(list)
-        visited = [False] * (n+1)
-
         for i in range(len(values)):
             A, B = equations[i]
-            dic[A].append(([values[i]], [B]))
-            dic[B].append(([1/values[i]], [A]))
+            dic[A].append((values[i], B))
+            dic[B].append((1/values[i], A))
+        print(dic)
+       
         
-        def dfs(visited, A, B, num):
-            if A not in dic or B not in dic:
-                return -1.0
+        def sol(visited, A, B, num):
             if A == B:
                 return num
-
-            visited.add(A)
-
-            idx = 0
-            for _, e in dic[A]:
-                if e == B:
-                    break
-                idx += 1
-            new, node = dic[A][idx]
-            num *= new
-            if node not in visited:
-                dfs(node, B, num)           
+            
+            for new_v, node in dic[A]:
+                if not visited[node]:
+                    visited[node] = True
+                    num *= new_v
+                    ans = sol(visited, node, B, num)
+                    if ans != -1:
+                        visited[node] = False
+                        return ans
+                    num /= new_v ## else 문 제거하고 이부분 추가해주니까 통과했다!!! (조언주신 분께 매우 큰절)
+            return -1            ## visited 방문을 True, False 처럼 처리해주듯이 num도 해당이 안될때 처리를해줘야하는거였다.
             
         res = []
+        
         for C, D in queries:
+            visited = {k: False for k in dic.keys()}
             num = 1.0
-            ans = dfs(set([]), C, D, num)
-            res.append(ans)
+            if C not in dic or D not in dic:
+                res.append(-1.0)
+                continue
+            else:           
+                visited[C] = True
+                ans = sol(visited, C, D, num)
+                visited[C] = False
+                res.append(ans)
             
         return res
